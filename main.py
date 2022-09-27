35b1745c7fa276637eeef8be4755210bac641b02
Flávia <113394221+flaviaperelberg@users.noreply.github.com>
2022-09-27 16:14:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+# Atividade verificadora de aprendizagem: faça um programa que peça um número e então mostre a mensagem - O número informado foi [número].
+
+numero = int(input('Informe um número:\n'))
+print('O número informado foi: ', numero)
